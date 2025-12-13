82d232e57a7824981f253d9545424196d9a13c6e
Aaron Syed <metroholographix@gmail.com>
2025-12-13 17:56:01 +0100

    chore: add PM2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ridepro-api',
+      script: 'dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3001,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3001,
+      },
+    },
+  ],
+};
