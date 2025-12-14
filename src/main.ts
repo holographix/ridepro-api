4ab6c7dd94c5082c8b23f8c753dbf74720bc0767
Aaron Syed <metroholographix@gmail.com>
2025-12-14 11:19:16 +0100

    feat: configure CORS for production domain

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,16 @@ async function bootstrap() {
   );
 
   // Enable CORS
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://ridepro.pixelsmasher.io',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   // Setup Swagger documentation
   const config = new DocumentBuilder()
